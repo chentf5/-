d00a967f78225bce853e61ffe80eb7faa59e256f
chentf5 <31510380+chentf5@users.noreply.github.com>
2018-11-24 17:32:25 +0800

    Add files via upload

---
+++ Agenda/include/Path.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef PATH_HPP
+#define PATH_HPP
+
+class Path {
+public:
+	/**
+	 * user.csv path
+	 */
+	static const char *userPath;
+
+	/**
+	 * meeting.csv path
+	 */
+	static const char *meetingPath;
+};
+
+const char *Path::meetingPath = "C:/Users/IT/Desktop/ʵѵ/AGENDA/AGENDA/tmp/meetings.csv";
+const char *Path::userPath = "C:/Users/IT/Desktop/ʵѵ/AGENDA/AGENDA/tmp/users.csv";
+
+#endif
